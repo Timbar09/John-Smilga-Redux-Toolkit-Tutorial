a2ce202be2cf8fc2f803e79da55dbb9f70b1ad52
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-27 07:51:33 +0200

    Add cartContainer.js and CartItem.js

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 
 const initialState = {
-  cartItems: [],
-  amount: 0,
+  cartItems: cartItems,
+  amount: 4,
   total: 0,
   isLoading: true,
 };
@@ -12,6 +13,6 @@ const cartSlice = createSlice({
   initialState,
 });
 
-console.log(cartSlice);
+// console.log(cartSlice);
 
 export default cartSlice.reducer;
